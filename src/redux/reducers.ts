de456284b4293447d21531a1e22c1c9e09f8a214
James Tharpe <james@brainbytescreative.com>
2019-06-15 10:23:32 -0400

    Beautify code format

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,29 +1,33 @@
-import { ITodo, schedule } from "../models/Todo";
+import { ITodo } from "../models/Todo";
 import { WhennerAction, WhennerActionType } from "./actions";
 import { defaultSettings, Settings } from "../models/Settings";
+import { schedule } from "../models/schedule";
 
-export function todos(state: ITodo[] = [], { dayStart: startTime, dayEnd: endTime}: Settings, action: WhennerAction) {
+export function todos(
+  state: ITodo[] = [],
+  { dayStart: startTime, dayEnd: endTime }: Settings,
+  action: WhennerAction
+) {
   let result = state;
   switch (action.type) {
     case WhennerActionType.CreateTodo:
-      result = [
-        ...state,
-        Object.assign({}, action.todo)
-      ];
+      result = [...state, Object.assign({}, action.todo)];
       break;
     case WhennerActionType.UpdateTodo:
-        result = state.map((todo) => {
-        // console.log("todos reducer, update", {todo, action });
-        return todo.id === action.todo.id ? Object.assign({}, action.todo) : todo
-      });
+      result = state.map(todo =>
+        todo.id === action.todo.id ? Object.assign({}, action.todo) : todo
+      );
       break;
-    // default:
-    //   return state;
+    default:
+      break;
   }
-  console.log("todos reducer completed", { oldState: state, newState: result });
-  return schedule({dayStart: startTime, dayEnd: endTime}, ...result);
+
+  return schedule({ dayStart: startTime, dayEnd: endTime }, ...result);
 }
 
-export function settings(state: Settings = defaultSettings, action: WhennerAction){
+export function settings(
+  state: Settings = defaultSettings,
+  action: WhennerAction
+) {
   return state;
 }
