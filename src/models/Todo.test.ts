d7de62cced21eda9af9d817ad45d956dda0c02a2
James Tharpe <james@brainbytescreative.com>
2019-06-15 10:22:44 -0400

    Increase Todo tests

--- src/models/Todo.test.ts
+++ src/models/Todo.test.ts
@@ -1,17 +1,67 @@
-import { Todo } from "./Todo";
+import { Todo, ITodo } from "./Todo";
 
-
-describe("A Todo", () => {
-  it("Converts to ITodo", () => {
-    const todo = new Todo({
+describe("The Todo Class", () => {
+  describe("Given a valid ITodo instance", () => {
+    const validITodoInstance: ITodo = {
       id: 1,
-      title: "Test",
-      description: "Test",
+      title: "ITodo Instance",
+      description: "This is a valid instance of the ITodo interface",
       estimate: 5,
       start: new Date(2019, 6, 2, 15, 9, 8, 7),
       done: false
-    })
+    };
 
-    
+    describe("When the instance is passed to the Todo constructor, it...", () => {
+      const todoFromValidITodo = new Todo(validITodoInstance);
+
+      it("Takes the properties of the ITodo instance", () => {
+        expect(todoFromValidITodo).toEqual(validITodoInstance);
+        expect(todoFromValidITodo).not.toBe(validITodoInstance);
+      });
+
+      it("Sets the End date based on the Start and Estimate", () => {
+        expect(todoFromValidITodo.end).toEqual(
+          new Date(2019, 6, 2, 15, 14, 8, 7)
+        );
+      });
+    });
+  });
+
+  describe("Given a falsey ITodo instance", () => {
+    const falseyITodoInstance: ITodo | null = null;
+
+    describe("When the instance is passed to the Todo constructor, it...", () => {
+      const todoFromFalseyITodo = new Todo(falseyITodoInstance);
+
+      it("Defaults to an ID of the current datetime", () => {
+        expect(todoFromFalseyITodo.id).toBeLessThanOrEqual(Date.now());
+        expect(todoFromFalseyITodo.id).toBeGreaterThanOrEqual(Date.now() - 100);
+      });
+
+      it("Defaults to an empty Title", () => {
+        expect(todoFromFalseyITodo.title).toBe("");
+      });
+
+      it("Defaults to an empty Description", () => {
+        expect(todoFromFalseyITodo.description).toBe("");
+      });
+
+      it("Defaults to a 60 minute Estimate", () => {
+        expect(todoFromFalseyITodo.estimate).toBe(60);
+      });
+
+      it("Defaults to a Start of the current datetime", () => {
+        expect(todoFromFalseyITodo.start.getTime()).toBeLessThanOrEqual(
+          Date.now()
+        );
+        expect(todoFromFalseyITodo.start.getTime()).toBeGreaterThanOrEqual(
+          Date.now() - 100
+        );
+      });
+
+      it("Defaults to NOT Done", () => {
+        expect(todoFromFalseyITodo.done).toBe(false);
+      });
+    });
   });
 });
--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -11,9 +11,9 @@ export interface ITodo {
 }
 
 export class Todo implements ITodo {
-  constructor(todo: ITodo) {
+  constructor(todo?: ITodo | null) {
     Object.assign(this, todo);
-    this.start = new Date(this.start); // JavaScript stores dates as strings when serializing
+    this.start = new Date(this.start || Date.now); // JavaScript stores dates as strings when serializing
   }
 
   id: number = Date.now();
@@ -24,9 +24,7 @@ export class Todo implements ITodo {
   done: boolean = false;
 
   get end() {
-    return moment(this.start)
-      .add(this.duration)
-      .toDate();
+    return Todo.calculateEnd(this);
   }
 
   set end(end: Date) {
@@ -36,16 +34,18 @@ export class Todo implements ITodo {
   }
 
   get duration() {
-    return moment.duration(this.estimate, "minutes");
+    return Todo.estimateToDuration(this);
   }
 
   adjustStart({ dayStart, dayEnd }: Settings) {
     dayStart = moment.duration(dayStart);
     dayEnd = moment.duration(dayEnd);
+    
     const earliest = moment(this.start)
       .startOf("day")
       .add(dayStart)
       .toDate();
+
     if (this.start < earliest) {
       this.start = earliest;
     }
@@ -63,28 +63,14 @@ export class Todo implements ITodo {
       this.adjustStart({ dayStart, dayEnd });
     }
   }
-}
 
-export function schedule(
-  { dayStart: startTime, dayEnd: endTime }: Settings,
-  ...todos: ITodo[]
-): ITodo[] {
-  const result = todos.map(todo => new Todo(todo));
-  const notDone = result
-    .filter(todo => !todo.done)
-    .sort((a, b) => a.start.getTime() - b.start.getTime());
-
-  const lastIndex = notDone.length - 1;
-  if (lastIndex === -1) return result;
-
-  notDone[0].start = new Date();
-  for (let i = 0; i <= lastIndex; i++) {
-    const current = notDone[i];
-    current.adjustStart({ dayStart: startTime, dayEnd: endTime });
-    if (i < lastIndex) {
-      notDone[i + 1].start = current.end;
-    }
+  static estimateToDuration({ estimate }: { estimate: number}){
+    return moment.duration(estimate, "minutes");
   }
 
-  return result;
+  static calculateEnd({ start, estimate }: ITodo) {
+    return moment(start)
+      .add(Todo.estimateToDuration({ estimate }))
+      .toDate();
+  }
 }
