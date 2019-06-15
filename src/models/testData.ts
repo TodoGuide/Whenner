e8033e7e0ebb52678c14d9823e2232f4b1cb3355
James Tharpe <james@brainbytescreative.com>
2019-06-15 10:23:09 -0400

    Add test data for convenience

---
+++ src/models/testData.ts
@@ -0,0 +1,19 @@
+
+export const oneHourTodo = {
+  id: 1,
+  title: "One hour todo",
+  description: "This is a one hour Todo",
+  estimate: 60,
+  start: new Date(2019, 6, 15, 9, 46, 45, 44),
+  done: false
+};
+
+
+export const twoHourTodo = {
+  id: 1,
+  title: "Two hour todo",
+  description: "This is two hour Todo",
+  estimate: 120,
+  start: new Date(2019, 6, 15, 9, 46, 45, 44),
+  done: false
+};
