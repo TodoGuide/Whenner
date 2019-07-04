a7ad73b95e3ceb0118f204b070336cada4d45734
James Tharpe <james@brainbytescreative.com>
2019-07-04 16:43:35 -0400

    Optimize todos reducer

--- src/redux/reducers/todos.ts
+++ src/redux/reducers/todos.ts
@@ -12,14 +12,8 @@ export function todos(
   let result = todos;
   switch (action.type) {
     case WhennerActionType.InsertTodoSuccess:
-      result = todos.map(todo => new Todo(todo));
-      result.push(new Todo(action.todo));
-      break;
     case WhennerActionType.UpdateTodoSuccess:
-      console.log("UpdateTodoSuccess", action.todo);
-      result = todos.map(todo =>
-        todo.id === action.todo.id ? new Todo(action.todo) : todo
-      );
+      // No need to calculate state because todos will be reloaded by thunk
       break;
     case WhennerActionType.LoadTodosSuccess:
       result = action.todos;
