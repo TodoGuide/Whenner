d1ba562cf06ecc18f95746e1109815a4a5279583
James Tharpe <jimmy.tharpe@gmail.com>
2019-07-05 15:41:06 -0400

    Spellin

--- src/models/Chronotype.ts
+++ src/models/Chronotype.ts
@@ -16,18 +16,3 @@ export const defaultChronotype: Chronotype = {
   start: moment.duration("7:15"), // 7:15am
   end: moment.duration("20:00") // 7:00pm
 };
-
-/*
-
-Why the term "Chronality"?
-
-Chronal means "of or relating to time".
-
-Diurnality: Behavior characterized by activity during daytime.
-Nocturnality: Behavior characterized by activity during nighttime.
-Metaturnality: Behaviour characterized by sporadic, irregular intervals of activity during the day or night.
-
-AFAICT, there is no word meaning "the time period characterized by activity" that is independent of
-the specific timeperiod, so "chronality" seemed to make sense as a semi-madeup word.
-
-*/
