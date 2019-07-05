d3e36f684ed0a3d61db7b2351a55a90bb5c0d679
James Tharpe <jimmy.tharpe@gmail.com>
2019-07-05 17:16:56 -0400

    Add utility functions for Chronotype

--- src/models/Chronotype.ts
+++ src/models/Chronotype.ts
@@ -1,4 +1,4 @@
-import * as moment from "moment";
+import moment from "moment";
 import { Duration } from "moment";
 
 /**
@@ -16,3 +16,37 @@ export const defaultChronotype: Chronotype = {
   start: moment.duration("7:15"), // 7:15am
   end: moment.duration("20:00") // 7:00pm
 };
+
+export function lengthInMinutes({ start, end }: Chronotype) {
+  return end.asMinutes() - start.asMinutes();
+}
+
+/**
+ * Calculates when the day starts for the specified Chronotype and date
+ *
+ * @export
+ * @param {Date} date
+ * @param {(Chronotype | { start: Duration })} { start }
+ * @returns
+ */
+export function chronotypeDayStart(date: Date, { start }: Chronotype | { start: Duration }){
+  return moment(date)
+    .startOf("day")
+    .add(start)
+    .toDate();
+}
+
+/**
+ * Calculates when the day ends for the specified Chronotype and date
+ *
+ * @export
+ * @param {Date} date
+ * @param {(Chronotype | { end: Duration })} { end }
+ * @returns
+ */
+export function chronotypeDayEnd(date: Date, { end }: Chronotype | { end: Duration }){
+  return moment(date)
+    .startOf("day")
+    .add(end)
+    .toDate();
+}
