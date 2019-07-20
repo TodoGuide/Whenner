ff140891320a822fd61cbc7b8e4b4d94ba3e008d
James Tharpe <james@brainbytescreative.com>
2019-07-20 19:49:41 -0400

    Convert between task and appointment

--- src/models/schedule.test.ts
+++ src/models/schedule.test.ts
@@ -1,18 +1,27 @@
-import { ITask } from "./Task";
+import { ITask, Task } from "./Task";
 import { customMatchers } from "../test/matchers";
 import { Time, addHour } from "./time";
-import { Appointment } from "./Appointment";
+import { Appointment, IAppointment } from "./Appointment";
+import { toAppointment, Schedule, toTask } from "./Schedule";
 
-// describe('A Schedule', () => {
-  
-// });
+describe("A Schedule", () => {
+  describe("Given a task that can't completed before an appointment", () => {
+    let schedule: Schedule;
+    const task: ITask = {};
+    const appointment: IAppointment = {};
 
-describe('The toAppointment method', () => {
+    beforeEach(() => {
+      schedule = new Schedule({ appointments: [appointment], tasks: [task] });
+    });
+  });
+});
+
+describe("The toAppointment method", () => {
   beforeEach(() => {
     jasmine.addMatchers(customMatchers);
     Time.set(new Date(2019, 6, 20, 13, 1, 0, 0)); // 2019-07-20 at 1:01PM
   });
-  
+
   it("Converts a Task to an appointment", () => {
     const task: ITask = {
       id: 1,
@@ -20,7 +29,7 @@ describe('The toAppointment method', () => {
       description: "description",
       estimate: 60,
       priority: Time.now()
-    }
+    };
 
     const expectedAppointment = new Appointment({
       id: 1,
@@ -30,6 +39,41 @@ describe('The toAppointment method', () => {
       end: addHour(Time.current())
     });
     const actualAppointment = toAppointment(task);
-    expect(actualAppointment).toEqual(expectedAppointment);
+    expect(actualAppointment).toEqual({
+      ...expectedAppointment,
+      estimate: 60,
+      priority: Time.now()
+    });
+  });
+});
+
+describe("The toTask method", () => {
+  beforeEach(() => {
+    jasmine.addMatchers(customMatchers);
+    Time.set(new Date(2019, 6, 20, 13, 1, 0, 0)); // 2019-07-20 at 1:01PM
+  });
+
+  it("Converts an Appointment to a task", () => {
+    const appointment: IAppointment = {
+      id: 1,
+      title: "title",
+      description: "description",
+      start: Time.current(),
+      end: addHour(Time.current())
+    };
+
+    const expectedTask = new Task({
+      id: 1,
+      title: "title",
+      description: "description",
+      priority: Time.now(),
+      estimate: 60
+    });
+    const actualTask = toTask(appointment);
+    expect(actualTask).toEqual({
+      ...expectedTask,
+      estimate: 60,
+      priority: Time.now()
+    });
   });
 });
--- src/models/schedule.ts
+++ src/models/schedule.ts
@@ -1,8 +1,6 @@
-import { ITask } from "./Task";
-import { IAppointment, Appointment } from "./Appointment";
-import { inPriorityOrder } from "./Todo";
+import { ITask, defaultTasks, Task } from "./Task";
+import { IAppointment, Appointment, defaultAppointments } from "./Appointment";
 import { Start } from "./time";
-
 
 export interface ISchedule {
   appointments: IAppointment[];
@@ -13,22 +11,37 @@ export class Schedule implements ISchedule {
   appointments: IAppointment[];
   tasks: ITask[];
 
-  constructor({appointments, tasks}: ISchedule){
+  constructor({ appointments, tasks }: ISchedule) {
     this.appointments = appointments;
     this.tasks = tasks;
   }
 
-  buildSchedule(){
+  buildSchedule() {
     const scheduledTasks = this.tasks.map(task => new Appointment(task));
-    const allItems = [ ...this.appointments, ...scheduledTasks ]
-    const all = inStartOrder(...allItems);
-  } 
+    const allItems = [...this.appointments, ...scheduledTasks];
+    return inStartOrder(...allItems);
+  }
 }
+
+export type TaskOrAppointment = ITask | IAppointment;
 
 export function inStartOrder<T extends Start>(...starts: T[]) {
   return starts.sort((a, b) => a.start.getTime() - b.start.getTime());
 }
 
-export function toAppointment(task: ITask){
-  return new Appointment(task);
+export function toAppointment(task: ITask) {
+  return new Appointment({ ...task, start: new Date(task.priority) });
 }
+
+export function toTask(appointment: IAppointment) {
+  return new Task({
+    ...appointment,
+    priority: appointment.start.getTime(),
+    estimate: Task.periodToEstimate(appointment)
+  });
+}
+
+export const defaultSchedule: ISchedule = {
+  tasks: defaultTasks,
+  appointments: defaultAppointments
+};
