3b2e51a72194bb5c5c9ce3efce2c4c9011082435
James Tharpe <james.tharpe1@t-mobile.com>
2019-07-23 01:14:14 -0400

    WIP: Schedule model

---
+++ src/models/Schedule.test.ts
@@ -0,0 +1,90 @@
+import { Appointment } from "./Appointment";
+import { customMatchers } from "./../test/matchers";
+import { Schedule } from "./Schedule";
+import { oneHourTask, twoHourTask, threeHourTask } from "./../test/data";
+import { defaultChronotype, Chronotype } from "./Chronotype";
+import { Task } from "./Task";
+import { Time, add30Minutes, addHour } from "./time";
+// describe("A Schedule", () => {
+
+// });
+
+describe("Schedule Mutations", () => {
+  beforeEach(() => {
+    jasmine.addMatchers(customMatchers);
+    Time.set(new Date(2019, 6, 5, 12, 0, 0, 0)); // 2019-07-05 at Noon
+  });
+
+  describe("Given two overlapping incomplete tasks and one completed task", () => {
+    let incompleteTask1: Task, incompleteTask2: Task, completedTask: Task;
+    beforeEach(() => {
+      incompleteTask1 = new Task({ ...oneHourTask });
+      incompleteTask2 = new Task({
+        ...twoHourTask,
+        start: add30Minutes(oneHourTask.start)
+      });
+      completedTask = new Task({
+        ...threeHourTask,
+        start: add30Minutes(oneHourTask.start)
+      });
+      completedTask.completed = Task.calculateEnd(completedTask);
+    });
+
+    describe("When stackTasks is called, it...", () => {
+      // let stackedTasks: ITask[];
+      beforeEach(() => {
+        Schedule.mutations.stackTasks(
+          Time.current(),
+          incompleteTask1,
+          incompleteTask2
+        );
+      });
+
+      it("Prioritizes the higher priority item for the given date", () => {
+        expect(incompleteTask1.priority).toEqual(Time.now());
+      });
+
+      it("Sets the lower priority task start to immedietly follow the higher priority task end", () => {
+        expect(incompleteTask2.start).toEqual(incompleteTask1.end);
+      });
+
+      it("Does NOT modify the completed task", () => {
+        expect(completedTask.start).toEqual(add30Minutes(oneHourTask.start));
+      });
+    });
+
+    describe("AND an appointment that overlaps the incomplete tasks", () => {
+      let overlappingAppointment: Appointment;
+      beforeEach(() => {
+        overlappingAppointment = new Appointment({
+          ...incompleteTask1,
+          start: add30Minutes(incompleteTask1.start),
+          priority: add30Minutes(incompleteTask1.start).getTime(),
+          end: add30Minutes(incompleteTask1.end)
+        });
+      });
+
+      describe("When scheduleTasks is called, it...", () => {
+        beforeEach(() => {
+          Schedule.mutations.scheduleTasks(
+            new Chronotype(),
+            [overlappingAppointment],
+            [incompleteTask1, incompleteTask2, completedTask]
+          );
+        });
+
+        it("Deprioritizes first task to start immedietly after the appointment", () => {
+          expect(incompleteTask1.start).toEqual(overlappingAppointment.end);
+        });
+
+        it("Deprioritizes second task to start immedietly after the first task", () => {
+          expect(incompleteTask2.start).toEqual(incompleteTask1.end);
+        });
+
+        it("Does NOT modify the completed task", () => {
+          expect(completedTask.start).toEqual(add30Minutes(oneHourTask.start));
+        });
+      });
+    });
+  });
+});
--- src/models/Schedule.ts
+++ src/models/Schedule.ts
@@ -1,10 +1,76 @@
-import { IAppointment, defaultAppointments } from "./Appointment";
-import { ITask, defaultTasks } from "./Task";
-
+import { IAppointment, defaultAppointments, Appointment } from "./Appointment";
+import { ITask, defaultTasks, Task } from "./Task";
+import { Chronotype, IChronotype } from "./Chronotype";
+import { inStartOrder, Start, laterOf, Time, periodsOverlap } from "./time";
+import { inPriorityOrder, Todo } from "./Todo";
 
 export interface ISchedule {
-  appointments: IAppointment[];
-  tasks: ITask[];
+  readonly appointments: IAppointment[];
+  readonly tasks: ITask[];
+}
+
+export class Schedule implements ISchedule {
+  readonly appointments: Appointment[];
+  readonly tasks: Task[];
+  readonly chronotype: Chronotype;
+
+  constructor(chronotype: IChronotype, { appointments, tasks }: ISchedule) {
+    this.chronotype = new Chronotype(chronotype);
+    this.appointments = inStartOrder(
+      ...appointments.map(appointment => new Appointment(appointment))
+    );
+    this.tasks = Schedule.mutations.stackTasks(
+      this.chronotype.startOf(Time.current()),
+      ...tasks.map(task => new Task(task))
+    );
+  }
+
+  static readonly mutations = {
+    stackTasks: function(start: Date, ...tasks: Task[]) {
+      let lastIncomplete: Task | undefined = undefined;
+      for (let i = 0; i < tasks.length; i++) {
+        const previous = tasks[i - 1];
+        const current = tasks[i];
+        lastIncomplete =
+          previous && previous.completed ? lastIncomplete : previous;
+        if (!current.completed) {
+          current.priority = laterOf(
+            start,
+            (lastIncomplete || { end: start }).end
+          ).getTime();
+        }
+      }
+
+      return inPriorityOrder(...tasks);
+    },
+
+    scheduleTasks: function(
+      chronotype: Chronotype,
+      appointments: Appointment[],
+      tasks: Task[]
+    ) {
+      const todos = inPriorityOrder(...[...appointments, ...tasks]);
+      for (let i = 0; i < todos.length; i++) {
+        const isAppointment = (todo: any) => appointments.indexOf(todo, i) >= 0;
+        const current = todos[i];
+        const next = todos[i + 1];
+
+        if (current.completed || isAppointment(current)) {
+          // Don't change completed items or appointments
+          continue;
+        }
+
+        // `current` is an incomplete Task
+
+        if (next && periodsOverlap(current, next)) {
+          const nextStartDate = isAppointment(next) ? next.end : current.start;
+          const currentTaskIndex = tasks.indexOf(<any>current);
+          const remainingTasks = tasks.slice(currentTaskIndex);
+          Schedule.mutations.stackTasks(nextStartDate, ...remainingTasks);
+        }
+      }
+    }
+  };
 }
 
 export const defaultSchedule: ISchedule = {
