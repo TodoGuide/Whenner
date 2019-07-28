e69d9369a5b120981b5b1e18cb294a6054178670
James Tharpe <james.tharpe1@t-mobile.com>
2019-07-28 19:43:25 -0400

    Documents priority funcs

--- src/models/Priority.ts
+++ src/models/Priority.ts
@@ -1,13 +1,28 @@
+/**
+ * Something with a numeric priority.
+ */
 export interface Priority {
   readonly priority: number;
 }
 
+/**
+ * A function that takes an object and returns a numeric priority
+ */
 export interface Prioritizer<T = {}> { 
     (item: T): number;
 }
 
+/**
+ * Returns a numeric priority for the given Priority object
+ * @param item The item to have its priority determined
+ */
 export const prioritizer: Prioritizer<Priority> = (item) => item.priority;
 
+/**
+ * The given set of objects, ordered by their numeric priority
+ * @param prioritizer The Prioritizer function
+ * @param priorities The items to prioritize
+ */
 export function prioritize<T>(prioritizer: Prioritizer<T>, ...priorities: T[]) {
   return priorities.sort((a, b) => prioritizer(a) - prioritizer(b));
 }
