290a1fece1270c8a65ed31a3a9c53bb08189876e
James Tharpe <james.tharpe1@t-mobile.com>
2019-07-28 22:15:59 -0400

    Remove start from task

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,6 +1,6 @@
-import { StartEstimated } from "./time/Estimated";
+import { Estimated } from "./time/Estimated";
 import { Todo } from "./Todo";
 import { Priority } from "./Priority";
 
-export interface Task extends Todo, Priority, StartEstimated {
+export interface Task extends Todo, Priority, Estimated {
 }
