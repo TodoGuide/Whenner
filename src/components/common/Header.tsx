627793e84ac1c8b6ee156ab104159519ed699f42
James Tharpe <jimmy.tharpe@gmail.com>
2020-03-15 13:56:54 -0400

    Add secondary nav

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,30 +2,63 @@
 // Copyright (C) 2019  James Tharpe
 
 import React from "react";
-import Jumbotron from "react-bootstrap/Jumbotron";
 import { LinkContainer } from "react-router-bootstrap";
 import Nav from "react-bootstrap/Nav";
+import { Navbar, Button, Form, FormControl } from "react-bootstrap";
 
 const Header: React.FC = () => (
   <div>
-    <Jumbotron>
-      <h1 className="App-title">Whenner!</h1>
-      <p>Answer "when're you gonna be done" like a Winner!</p>
-    </Jumbotron>
-    <Nav fill variant="tabs" activeKey={window.location.pathname}>
-      <Nav.Item as={LinkContainer} to="/tasks">
-        <Nav.Link active={false}>Tasks</Nav.Link>
-      </Nav.Item>
-      <Nav.Item as={LinkContainer} exact to="/calendar">
-        <Nav.Link active={false}>Calendar</Nav.Link>
-      </Nav.Item>
-      <Nav.Item as={LinkContainer} exact to="/settings">
-        <Nav.Link active={false}>Settings</Nav.Link>
-      </Nav.Item>
-      <Nav.Item as={LinkContainer} exact to="/about">
-        <Nav.Link active={false}>About</Nav.Link>
-      </Nav.Item>
-    </Nav>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="/">Whenner</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Item as={LinkContainer} to="/tasks">
+            <Nav.Link active={false}>Tasks</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as={LinkContainer} exact to="/calendar">
+            <Nav.Link active={false}>Calendar</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Nav>
+          <Nav.Item as={LinkContainer} exact to="/settings">
+            <Nav.Link active={false}>Settings</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as={LinkContainer} exact to="/about">
+            <Nav.Link active={false}>About</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    <Navbar>
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto justify-content-center">
+          <Nav.Item>
+            <Button variant="primary">New Task</Button>
+          </Nav.Item>
+        </Nav>
+        <Nav className="mr-auto justify-content-center">
+          <Nav.Item>
+            <span className="mr-3 font-weight-bold">Filter: </span>
+            <Form.Check type="checkbox" inline label="Incomplete" checked />
+            <Form.Check type="checkbox" inline label="Complete" />
+            <Form.Check type="checkbox" inline label="Canceled" />
+          </Nav.Item>
+        </Nav>
+        <Nav>
+          <Nav.Item>
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   </div>
 );
 
